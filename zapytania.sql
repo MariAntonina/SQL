532bd598455d5036c92a71b111388461d695a1e8
MariAntonina <maryhuzarek@gmail.com>
2017-09-11 18:13:37 +0200

    Est qu'elle superbe?

---
+++ zapytania.sql
@@ -0,0 +1,23 @@
+#SELECT question FROM questions where correctAnswer = 'a'; 
+#1
+#SELECT * FROM questions where id = 15; #2
+#2
+#SELECT question FROM questions where correctAnswer = 'a'; #3
+#3
+#SELECT question, answerb from questions where correctAnswer = "b" and year >= 2008;
+#4
+#select question, correctAnswer from questions where question like 'Jak%';
+#5
+#select * from questions where correctAnswer = "b" or correctAnswer = "c";
+#6
+#select question from questions where category = "programowanie"; 
+#7
+#select question from questions where question like "%klaser";
+#8
+#select question from questions order by question asc;
+#9
+#select * from questions where id >= 6 AND id <= 10; 
+#10
+#select * from questions where year != 2017;
+#11
+#select question from questions where category = 'OOP' and category = 'algorytmika';
